5684c4b9fea30820365dc41210da6101ff4e47ae
Haashir-Shakeel <david10james99@gmail.com>
2022-10-04 15:57:47 +0500

    added allcontacts

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Contact
 # Create your views here.
+
+def allcontact(request):
+    contact = Contact.objects.all()
+    context= {
+        'contacts': contact
+    }
+    return render(request, 'index.html', context)
+
+
+
