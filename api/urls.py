8aa56837a8ab4821ce60dfa08b30f3dc513b996e
Haashir-Shakeel <david10james99@gmail.com>
2022-10-05 16:20:01 +0500

    added detailview and deleteview

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.allcontact, name="allcontact"),
-    path('formsubmit',views.formsubmit,name="formsubmit")
+    path('formsubmit',views.formsubmit,name="formsubmit"),
+    path('detailContact/<str:pk>/',views.detailContact,name="detailContact"),
+    path('deleteContact/<str:pk>/',views.deleteContact,name="deleteContact")
     ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Contact
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 # Create your views here.
 
 def allcontact(request):
@@ -20,6 +20,19 @@ def formsubmit(request):
     newcontact = Contact.objects.create(name=name,email=email,phone=phone)
     newcontact.save()
     return HttpResponseRedirect('/')
+
+def detailContact(request,pk):
+    data = Contact.objects.get(id=pk)
+    context = {
+        "data":data
+    }
+    return render(request, 'detail.html', context)
+
+def deleteContact(request,pk):
+    contact = Contact.objects.get(id=pk)
+    contact.delete()
+    return HttpResponseRedirect('/')
+
 
 
 
